3736ed58a06210e954041198df0a2d3a9041bc58
dariyasorokinaav <dariyasorokinaav@yandex.ru>
2026-03-05 17:00:57 +0300

    changed db settings

--- Generator/Generator/settings.py
+++ Generator/Generator/settings.py
@@ -119,27 +119,18 @@ WSGI_APPLICATION = 'Generator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'generatordb',
-#         'USER': 'generator_user',
-#         'PASSWORD': 'StrongPass123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'generatordb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'USER': 'generator_user',
+        'PASSWORD': 'StrongPass123',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
+
+
 
 
 
